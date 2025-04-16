f107c70ea0fe5cffd538148273cee9cefd942f02
Md Asad Chowdhary <asad@suyena.com>
2025-04-16 16:12:49 +0600

    Fix styles, Analytics and Insights.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,13 +4,13 @@ import ProfileForm from './components/ProfileForm';
 import ProfileList from './components/ProfileList'; 
 import ProfileNav from './ProfileNav';
 import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 function HomePage() {
   return (
     <>
     <ProfileNav/>
-    <SpeedInsights/>
+    <SpeedInsights/> 
     <Analytics/></>
   );
 }
