9f83ee7b55f25ca9d1317e876e23eef6505ea79f
Md Asad Chowdhary <asad@suyena.com>
2025-04-16 12:44:20 +0600

    Update ProfileForm.jsx
    Colored error

--- client/src/components/ProfileForm.jsx
+++ client/src/components/ProfileForm.jsx
@@ -135,8 +135,9 @@ const ProfileForm = () => {
           {renderInputField("personal", "address", "text", true, handlePersonalChange, personal.address)}
           {renderInputField("personal", "email", "email", false, handlePersonalChange, personal.email)}
           {renderFileInput("personal", "personalImage", handlePersonalImageChange)}
-        <p style="color:red; font-weight:bold">Use photo less than 1MB</p>
-        </div>
+<p style={{ color: 'red', fontWeight: 'bold' }}>Use photo less than 1MB</p>
+          
+       </div>
 
         {/* Nominee Information Section */}
         <div className="form-section">
@@ -150,7 +151,8 @@ const ProfileForm = () => {
            {renderInputField("nominee", "address", "text", true, handleNomineeChange, nominee.address)}
            {renderInputField("nominee", "email", "email", false, handleNomineeChange, nominee.email)}
            {renderFileInput("nominee", "nomineeImage", handleNomineeImageChange)}
-       <p style="color:red; font-weight:bold">Use photo less than 1MB</p>
+<p style={{ color: 'red', fontWeight: 'bold' }}>Use photo less than 1MB</p>
+          
         </div>
 
         {/* Use the submit button class, remove inline style */}
